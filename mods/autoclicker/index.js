1ef1822e5da9ae9b6814fc0ce35b9fad9a48abfc
askaye <95990100+deprecatedbrain@users.noreply.github.com>
2024-05-30 19:29:23 -0400

    Add autoclicker script

---
+++ mods/autoclicker/index.js
@@ -0,0 +1,16 @@
+let CURRENT_CPS = 20;
+let TOGGLE_KEYBIND = 22;
+let toggled = false;
+
+ModAPI.addEventListener("key", event => {
+    if (event.key === TOGGLE_KEYBIND) {
+        toggled = !toggled;
+    }
+});
+
+let autoClickInterval = setInterval(() => {
+    if (toggled) {
+        console.log("click");
+        ModAPI.clickMouse();
+    }
+}, 1000 / CURRENT_CPS);
